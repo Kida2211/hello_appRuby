7b534c16426f4be0bad414e84fa9fcbc27345f53
Kida2211 <74393925+Kida2211@users.noreply.github.com>
2024-05-20 00:58:40 +0000

    Add build script

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup_path assert_response :success end
+    get signup_path
+    assert_response :success
   end
 end
